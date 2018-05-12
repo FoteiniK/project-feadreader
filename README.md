94e96eb2fc7c3e1971ba502513c789a199e1840c
Foteini Kollia <Foteinikollia@gmail.com>
2018-05-12 21:32:08 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# project-feadreader
+Fifth project for Udacity's FEND nanodegree program.
+
+# What this project is about
+In this project we are given a web-based application that reads RSS feeds.We use Jasmine to write a number of tests against a pre-existing application. These will test the underlying business logic of the application as well as the event handling and DOM manipulation.
+
+
+## My steps to complete the project
+1. I took the JavaScript Testing [course](https://www.udacity.com/course/ud549)
+2. I downloaded the [required project assets](http://github.com/udacity/frontend-nanodegree-feedreader).
+3. After exploring the given material,I started implementing the tests one by one. After writing each test,I reviewed the functionality of the application within my browser and I made sure that each test passes before moving to the next one. 
+
+## How to run the tests
+ After cloning or downloading the file,use the index.html file to run the tests. The test's code is in feedreader.js file inside jasmine/spec folder.
+
+ If you want to see a live demo of the tests,visit the link provided on the bottom of this README.
+
+ After the page loads,all the tests pass. You can examine each test or each suite seperately by clicking on it. After passing,the test's name is green.
+
+##Libraries,frameworks and tools used in this project
+* jQuery
+* Jasmine
+* ESLint
+
+
+---
+   ###### GitHub page: 
---
+++ jasmine/spec/feedreader.js
@@ -0,0 +1,130 @@
+/* jshint ignore:start */
+'use strict';
+/* jshint ignore:end */
+/*jshint esversion: 6 */
+/*globals $:false */
+/*global  expect,it,describe,allFeeds,beforeEach,loadFeed*/
+
+
+/* feedreader.js
+ *
+ * This is the spec file that Jasmine will read and contains
+ * all of the tests that will be run against our application.
+ */
+/* We're placing all of our tests within the $() function,
+ * since some of these tests may require DOM elements. We want
+ * to ensure they don't run until the DOM is ready.
+ */
+
+$(() => {
+	/* This is our first test suite - a test suite just contains
+	 * a related set of tests. This suite is all about the RSS
+	 * feeds definitions, the allFeeds letiable in our application.
+	 */
+	describe('RSS Feeds', () => {
+		/* This is our first test - it tests to make sure that the
+		 * allFeeds letiable has been defined and that it is not
+		 * empty. Experiment with this before you get started on
+		 * the rest of this project. What happens when you change
+		 * allFeeds in app.js to be an empty array and refresh the
+		 * page?
+		 */
+		it('are defined', () => {
+			expect(allFeeds).toBeDefined();
+			expect(allFeeds.length).not.toBe(0);
+		});
+
+
+		/* This is our second test-it loops through each feed
+		 * in the allFeeds object and ensures it has a URL defined
+		 * and that the URL is not empty.
+		 */
+		it('URL is defined and is not empty', () => {
+			allFeeds.forEach((allFeeds) => {
+				expect(allFeeds.url).toBeDefined();
+				expect(allFeeds.url).not.toBe('');
+			});
+
+		});
+
+
+		/* This is our third test-it loops through each feed
+		 * in the allFeeds object and ensures it has a name defined
+		 * and that the name is not empty.
+		 */
+		it('name is defined and is not empty', () => {
+			allFeeds.forEach((allFeeds) => {
+				expect(allFeeds.url).toBeDefined();
+				expect(allFeeds.url).not.toBe('');
+			});
+		});
+
+	});
+
+
+	/* This is our second test suite -it is about the menu's element visibility and
+	 *that menu-icon's funcionality*/
+	describe('The menu', () => {
+
+		/*This tests ensures the menu element is
+		 * hidden by default. */
+		it('hides the menu element by default', () => {
+			expect($('body').hasClass('menu-hidden')).toBe(true);
+		});
+
+		/* This test ensures the menu changes
+		 * visibility when the menu icon is clicked.*/
+		it('appears the menu element when menu icon is clicked', () => {
+			$('.menu-icon-link').trigger('click');
+			expect($('body').hasClass('menu-hidden')).toBe(false);
+
+			$('.menu-icon-link').trigger('click');
+			expect($('body').hasClass('menu-hidden')).toBe(true);
+		});
+	});
+
+	/* This is our third test suite -it is about .feed container's entries after
+	the first time that loadFeed is invoked */
+	describe('Initial Entries', () => {
+		beforeEach((done) => {
+			loadFeed(0, () => {
+				done();
+			});
+		});
+
+
+		/*This test ensures tht when the loadFeed
+		 * function is called and completes its work, there is at least
+		 * a single .entry element within the .feed container.*/
+
+	});
+	it('ensures that .feedcontainer is not empty after loadFeed', () => {
+		expect($('.feed').children()).toBeDefined();
+
+	});
+
+
+	/* This is our third test suite -it is about loadFeeds content
+	 after loading	new feed */
+	describe('New Feed Selection', () => {
+
+
+		/* This test ensures that,when a new feed is loaded
+		 * by the loadFeed function that the content actually changes.
+		 */
+		let firstFeed, secondFeed;
+		beforeEach((done) => {
+			loadFeed(0);
+			firstFeed = allFeeds[0];
+			loadFeed(1);
+			secondFeed = allFeeds[1];
+			done();
+		});
+
+		it('changes the content after loading new feed', (done) => {
+			expect(firstFeed).not.toEqual(secondFeed);
+			done();
+		});
+	});
+
+})();
